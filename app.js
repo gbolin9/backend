fcecf0dd1f7df003c004d1c7ea89e942df7069a9
Gary Bolin <garybolin@ivytech.edu>
2026-04-20 19:54:30 -0400

    Create api route to get songs

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express")
+var cors = require('cors')
 
 const app = express()
+app.use(cors())
 const router = express.Router()
 
-app.listen(3000,function(){
-    console.log("Listening on port 3000")
+
+
+
+router.get("/songs",function(req,res){
+    const songs =[
+    {
+        title: "Love Shack",
+        artist: "The B-52s",
+        popularity: 10,
+        releaseDate: new Date(1989, 6, 20),
+        genre: ["pop-rock", "new wave", "surf rock"]
+    },
+
+    {
+        title: "Rockstar",
+        artist: "Nickleback",
+        popularirty: 10,
+        releaseDate: new Date(2006, 8, 14),
+        genre: ["Dad Rock", "Pop Rock", "Goated"]
+    }
+   
+    ]
+
+    res.json(songs)
 })
 
+app.use("/api",router)
+app.listen(3000)
