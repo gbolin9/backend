ec0af84d1e4f88b32a42caea926428166ed39437
Gary Bolin <garybolin@ivytech.edu>
2026-05-02 01:42:42 -0400

    added users and auth models and functionality

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const Song = require("./models/songs");
 var cors = require("cors")
+//const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
+const User = require("./models/users")
 
 const app = express();
 app.use(cors())
@@ -9,6 +12,78 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router();
+const secret = "supersecret"
+
+//creating a user
+router.post("/users", async(req,res) => {
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+    }
+
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+
+    })
+    try{
+        newUser.save()
+        res.sendStatus(201)
+    }
+    catch(err){
+        res.status(400).send(err)
+
+    }
+    
+})
+
+//Authenticate or login
+router.post("/auth", async(req,res)=>{
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+        return 
+    }
+    //try to find username in database and see if it matches
+    let user = await User.findOne({username : req.body.username})
+    
+        if(!user){
+            res.status(401).json({error: "Bad Username"})
+        }
+        else{
+            if(user.password != req.body.password){
+                res.status(401).json({error: "Bad Password"})
+            }
+            else{
+                username2 = user.username
+                const token = jwt.encode({username: user.username}, secret)
+                const auth = 1
+
+                res.json({
+                    username2,
+                    token:token,
+                    auth:auth
+                })
+            }
+        }
+    })
+
+    router.get("/status", async(req,res) => {
+        if(!req.headers["x-auth"]){
+            return res.status(401).jason({error: "Missing X-Auth"})
+        }
+
+        const token = req.headers["x-auth"]
+        try{
+            const decoded = jwt.decode(token,secret)
+
+            let users = User.find({}.filter("username status"))
+            res.json(users)
+        }
+        catch(ex){
+            res.status(401).json({error: "invalid jwt"})
+        }
+    })
+
 
 // Get list of all songs in the database
 router.get("/songs", async function(req, res) {
@@ -57,6 +132,22 @@ router.put("/songs/:id", async(req,res) => {
     }
 })
 
+router.delete("/songs/:id", async(req,res) =>{
+
+    try{
+        const song = await Song.findById(req.params.id)
+        console.log(song)
+        await Song.deleteOne({_id: song._id})
+        
+
+    }
+    catch(err){
+        res.status(400).send(err)
+
+    }
+
+})
+
 app.use("/api", router);
 
-app.listen(3000);
+app.listen(3000, () => console.log("server running"));
